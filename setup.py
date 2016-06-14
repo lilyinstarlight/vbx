e1a985d2948b3ec5b034a2c44e66960973193181
Foster McLane <fkmclane@gmail.com>
2016-06-14 16:10:13 -0400

    add skeleton files

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+from vbx import name, version
+
+setup(
+	name=name,
+	version=version,
+	description='virtual branch exchange for interfacing with Twilio',
+	license='MIT',
+	url='https://github.com/fkmclane/vbx',
+	author='Foster McLane',
+	author_email='fkmclane@gmail.com',
+	install_requires=['web.py'],
+	packages=find_packages(),
+	package_data={'': ['res/*.*']},
+	entry_points = {'console_scripts': ['vbx = vbx.main']},
+)
---
+++ vbx/__init__.py
@@ -0,0 +1,2 @@
+name = "vbx"
+version = "0.0a0"
