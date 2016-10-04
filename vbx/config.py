43ac1b28caa6c1aa9924febf5d2ad4515935d492
Foster McLane <fkmclane@gmail.com>
2016-10-04 10:11:21 -0400

    add a useful default config which shows how the flows work

--- vbx/config.py
+++ vbx/config.py
@@ -1,6 +1,20 @@
+import vbx.devices
+import vbx.flows
+
 # address to listen on
 addr = ('', 8080)
 
 # log locations
 log = '/var/log/vbx/vbx.log'
 httplog = '/var/log/vbx/http.log'
+
+# call flows (make sure they connect properly by key)
+calls = {
+    'call': vbx.flows.Device(devices=[vbx.devices.Browser], next='personal'),
+    'phone': vbx.flows.Dial(number='+18005555555'),
+}
+
+# message flows (make sure they connect properly by key)
+messages = {
+    'message': vbx.flows.Device(devices=[vbx.devices.Browser, vbx.devices.XMPP, vbx.devices.SMS]),
+}
