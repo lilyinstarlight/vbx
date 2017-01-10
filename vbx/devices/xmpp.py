4357c92186744eb2e90a331b5bdcdad6381ffae7
Foster McLane <fkmclane@gmail.com>
2017-01-10 11:32:12 -0500

    add working xmpp component

--- vbx/devices/xmpp.py
+++ vbx/devices/xmpp.py
@@ -1,5 +1,6 @@
 import asyncio
 import functools
+import importlib
 import queue
 import threading
 
@@ -16,19 +17,19 @@ class XMPP(vbx.Device):
         self.component = XMPPComponent(jid, secret, server, port, target)
 
     def online(self):
-        return self.component.online
+        return self.component.target_online
 
     def send(self, event, message, response):
-        self.component.send(event, message)
+        self.component.send_from_twilio(event, message)
 
 class XMPPComponent(slixmpp.ComponentXMPP):
     def __init__(self, jid, secret, server, port, target):
         self.target = target
-        self.queue = queue.Queue()
+        self.twilio_queue = queue.Queue()
 
         self.config = None
 
-        self.online = False
+        self.target_online = False
 
         self.thread = threading.Thread(target=functools.partial(self.component_thread, jid, secret, server, port), name='XMPPComponent')
         self.thread.start()
@@ -38,14 +39,12 @@ class XMPPComponent(slixmpp.ComponentXMPP):
 
         super().__init__(jid, secret, server, port)
 
-        self.add_event_handler('message', self.recv)
-        self.add_event_handler('got_offline', self.offline)
-        self.add_event_handler('got_online', self.online)
+        self.add_event_handler('session_start', self.session_start)
+        self.add_event_handler('message', self.recv_from_xmpp)
+        self.add_event_handler('got_offline', self.set_offline)
+        self.add_event_handler('got_online', self.set_online)
 
-        if not self.connect():
-            raise Exception('Could not connect to XMPP server')
-
-        self.send_presence_subscription(self.target)
+        self.connect():
 
         while True:
             self.process(timeout=0.5)
@@ -56,39 +55,41 @@ class XMPPComponent(slixmpp.ComponentXMPP):
             except queue.Empty:
                 pass
 
-    def create_config(self):
-        self.config = __import__('vbx.config')
-        self.config.revcontacts = {contact: number for number, contact in self.config.contacts.items()}
+    def session_start(self, data):
+        self.vbx_config = importlib.import_module('vbx.config')
+        self.vbx_config.revcontacts = {contact: number for number, contact in self.vbx_config.contacts.items()}
 
-        self.twilio_client = twilio.rest.Client(username=vbx.config.auth[0], password=vbx.config.auth[1])
+        self.twilio_client = twilio.rest.Client(username=self.vbx_config.auth[0], password=self.vbx_config.auth[1])
 
-    def recv(self, msg):
-        if not self.config:
-            self.create_config()
+        self.send_presence_subscription(self.target)
+
+    def recv_from_xmpp(self, msg):
+        if not self.vbx_config:
+            return
 
         to = msg['to'].node
 
-        if to in self.config.revcontacts:
-            to = self.config.revcontacts[to]
+        if to in self.vbx_config.revcontacts:
+            to = self.vbx_config.revcontacts[to]
 
-        self.twilio_client.messages.create(to, body=msg['body'], from_=vbx.config.number)
+        self.twilio_client.messages.create(to, body=msg['body'], from_=self.vbx_config.number)
 
-    def _send(self, event, target, msg):
-        if not self.config:
-            self.create_config()
+    def _send_from_twilio(self, event, msg):
+        if not self.vbx_config:
+            return
 
         from_ = event['From']
 
-        if from_ in self.config.contacts:
-            from_ = self.config.contacts[from_]
+        if from_ in self.vbx_config.contacts:
+            from_ = self.vbx_config.contacts[from_]
 
-        self.send_message(target, msg, mfrom=from_ + '@' + self.boundjid.domain)
+        self.send_message(self.target, msg, mfrom=from_ + '@' + self.boundjid.domain)
 
-    def offline(self):
-        self.online = False
+    def set_offline(self, data):
+        self.target_online = False
 
-    def online(self):
-        self.online = True
+    def set_online(self, data):
+        self.target_online = True
 
-    def send(self, event, target, msg):
-        self.queue.put((event, target, msg))
+    def send(self, event, msg):
+        self.twilio_queue.put((event, msg))
