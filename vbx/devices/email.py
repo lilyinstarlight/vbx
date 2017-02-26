7a70b81d44e6ab27d4bc71f9db09aa7941b04873
Foster McLane <fkmclane@gmail.com>
2017-02-26 17:05:30 -0500

    add basic sms gateway and contact lookup

--- vbx/devices/email.py
+++ vbx/devices/email.py
@@ -3,6 +3,7 @@ import smtplib
 from email.mime.text import MIMEText
 
 import vbx
+import vbx.config
 
 
 class Email(vbx.Device):
@@ -13,7 +14,12 @@ class Email(vbx.Device):
     def send(self, event, message, response):
         msg = MIMEText(message)
 
-        msg['Subject'] = 'SMS From {}'.format(event.from_)
+        if event.from_ in vbx.config.contacts:
+            from_ = vbx.config.contacts[events.from_]
+        else
+            from_ = events.from_
+
+        msg['Subject'] = 'SMS From {}'.format(from_)
         msg['From'] = self.from_
         msg['To'] = self.to
 
--- vbx/devices/sms.py
+++ vbx/devices/sms.py
@@ -1,9 +1,21 @@
 import vbx
+import vbx.config
 
 
 class SMS(vbx.Device):
     def __init__(self, number):
         self.number = number
+        self.last = None
 
     def send(self, event, message, response):
-        response.message('From: ' + event.from_ + '\n' + message, to=self.number)
+        if event.from_ == self.number and self.last:
+            response.message(message, to=self.last)
+        else:
+            self.last = event.from_
+
+        if event.from_ in vbx.config.contacts:
+            from_ = vbx.config.contacts[events.from_]
+        else
+            from_ = events.from_
+
+        response.message('From: ' + from_ + '\n' + message, to=self.number)
