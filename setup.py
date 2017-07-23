9aed09f82cd4bbb40504e729fc408fe4b08f2505
Foster McLane <fkmclane@gmail.com>
2017-07-23 16:24:15 -0400

    fix version searching

--- setup.py
+++ setup.py
@@ -1,7 +1,35 @@
 #!/usr/bin/env python3
+import os
+import re
+
 from setuptools import setup, find_packages
 
-from vbx import name, version
+
+name = None
+version = None
+
+
+def find(haystack, *needles):
+    regexes = [(index, re.compile("^{}\s*=\s*'([^']*)'$".format(needle))) for index, needle in enumerate(needles)]
+    values = ['' for needle in needles]
+
+    for line in haystack:
+        if len(regexes) == 0:
+            break
+
+        for rindex, (vindex, regex) in enumerate(regexes):
+            match = regex.match(line)
+            if match:
+                values[vindex] = match.groups()[0]
+                del regexes[rindex]
+                break
+
+    return values
+
+
+with open(os.path.join(os.path.dirname(__file__), 'vbx', '__init__.py'), 'r') as vbx:
+    name, version = find(vbx, 'name', 'version')
+
 
 setup(
     name=name,
