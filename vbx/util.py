56e7a895f65d3ed5ef52b86639b00f1cc2641e61
Foster McLane <fkmclane@gmail.com>
2019-04-07 21:05:50 -0400

    quote non-ascii characters in url

--- vbx/util.py
+++ vbx/util.py
@@ -7,6 +7,7 @@ api_base = 'https://api.twilio.com/2010-04-01'
 
 def get_media_url(url, default=None):
     url = urllib.parse.urlparse(url)
+    url = url._replace(path=urllib.parse.quote(url.path), params=urllib.parse.quote(url.params), query=urllib.parse.quote(url.query), fragment=urllib.parse.quote(url.fragment))
     if url.scheme and url.netloc:
         with urllib.request.urlopen(urllib.request.Request(url=url.geturl(), method='HEAD')) as response:
             if response.getheader('Content-Type').split('/', 1)[0] in ['image', 'video', 'audio']:
