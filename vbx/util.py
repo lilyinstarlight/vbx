2f7abaa70d0279c70db56bcebbbf92005b81416e
Foster McLane <fkmclane@gmail.com>
2019-07-20 12:59:24 -0400

    ignore media that gives an http error

--- vbx/util.py
+++ vbx/util.py
@@ -9,9 +9,12 @@ def get_media_url(url, default=None):
     url = urllib.parse.urlparse(url)
     url = url._replace(path=urllib.parse.quote(url.path), params=urllib.parse.quote(url.params), query=urllib.parse.quote(url.query), fragment=urllib.parse.quote(url.fragment))
     if url.scheme and url.netloc:
-        with urllib.request.urlopen(urllib.request.Request(url=url.geturl(), method='HEAD')) as response:
-            if response.getheader('Content-Type').split('/', 1)[0] in ['image', 'video', 'audio']:
-                return response.geturl()
+        try:
+            with urllib.request.urlopen(urllib.request.Request(url=url.geturl(), method='HEAD')) as response:
+                if response.getheader('Content-Type').split('/', 1)[0] in ['image', 'video', 'audio']:
+                    return response.geturl()
+        except (IOError, urllib.error.HTTPError):
+            pass
 
     return default
 
